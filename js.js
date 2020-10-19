8857eec0c74a15958cb97199db9fc8c86ef42335
ramisayeh <rami6sayeh@gmail.com>
2020-10-19 21:27:56 +0100

    update

--- js.js
+++ js.js
@@ -1,11 +1,11 @@
 function makeFlights16(city, flightdate1,  flightdate2, price, fds, bgimgurl ) {
    
     return   flight = {
-          city : city ,
-          flightdate1: flightdate1,
-          flightdate2: flightdate2,
-          fds : fds,
-           price: price,
+          "city" : city ,
+          "flightdate1": flightdate1,
+          "flightdate2": flightdate2,
+          "fds" : fds,
+           "price": price,
           bgimgurl : bgimgurl
       } 
       
@@ -51,27 +51,27 @@ function makeFlights16(city, flightdate1,  flightdate2, price, fds, bgimgurl ) {
     makeFlights16("new york", "23/10/2020", "24/10/2020", "$900", "first  class", "new york");
     x.push(flight);
 
-    var flight = [ { city: "berlin", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "first class", price: "$3500", "berlin"},
-     {city: "berlin", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "Business class", price: "$2400","berlin"} ,
-     {city: "berlin", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "economy class", price: "$950", "berlin"},
-     {city: "paris", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "first class", price: "$3400", "paris"},
-     {city: "paris", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "business class", price: "$2100",  "paris"},
-     {city: "paris", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "economy class", price: "$900", "paris"},
-     {city: "venice", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "first  class", price: "$4100","venice"},
-     {city: "venice", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "business  class", price: "$3000","venice"},
-     {city: "venice", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "economy  class", price: "$1100", "venice"},
-     {city: "tokyo", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "first  class", price: "$3100","tokyo"},
-    {city: "tokyo", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "business  class", price: "$2000","tokyo"},
-     {city: "tokyo", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "economy  class", price: "$1100", "tokyo"},
-     {city: "madrid", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "first  class", price: "$5100", "madrid"},
-     {city: "madrid", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "business  class", price: "#1900", "madrid"},
-     {city: "madrid", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "ecocnomy  class", price: "$1100", "madrid"},
-     {city: "Séoul", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "first  class", price: "$3100", "Séoul"},
-    {city: "Séoul", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "business  class", price: "$2100", "Séoul"},
-    {city: "Séoul", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "economy  class", price: "$850", "Séoul"},
-     {city: "new york", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "first  class", price: "$4100","new york"},
-    {city: "new york", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "business  class", price: "$3600","new york"},
-    {city: "new york", flightdate1: "23/10/2020", flightdate2: "24/10/2020", fds: "first  class", price: "$900", "new york"} ]
+    var flight = [ { "city": "berlin", "flightdate1": "23/10/2020", "flightdate2": "24/10/2020", "fds": "first class", "price": "$3500", "bgimgurl": "berlin"},
+     {"city": "berlin", "flightdate1": "23/10/2020","flightdate2": "24/10/2020","fds": "Business class", "price": "$2400", "bgimgurl": "berlin"} ,
+     {"city": "berlin", "flightdate1": "23/10/2020", "flightdate2": "24/10/2020", "fds": "economy class", "price": "$950","bgimgurl": "berlin"},
+     {"city": "paris", "flightdate1": "23/10/2020","flightdate2": "24/10/2020", "fds": "first class", "price": "$3400","bgimgurl": "paris"},
+     {"city": "paris", "flightdate1": "23/10/2020", "flightdate2": "24/10/2020", "fds": "business class", "price": "$2100","bgimgurl":  "paris"},
+     {"city": "paris","flightdate1": "23/10/2020", "flightdate2": "24/10/2020", "fds": "economy class", "price": "$900","bgimgurl": "paris"},
+     {"city": "venice", "flightdate1": "23/10/2020", "flightdate2": "24/10/2020", "fds": "first  class", "price": "$4100","bgimgurl": "venice"},
+     {"city": "venice", "flightdate1": "23/10/2020", "flightdate2": "24/10/2020", "fds": "business  class", "price": "$3000","bgimgurl": "venice"},
+     {"city": "venice",  "flightdate1": "23/10/2020", "flightdate2": "24/10/2020", "fds": "economy  class","price": "$1100","bgimgurl": "venice"},
+     {"city": "tokyo",  "flightdate1": "23/10/2020", "flightdate2": "24/10/2020", "fds": "first  class", "price": "$3100","bgimgurl": "tokyo"},
+    {"city": "tokyo",  "flightdate1": "23/10/2020", "flightdate2": "24/10/2020","fds": "business  class", "price": "$2000","bgimgurl": "tokyo"},
+     {"city": "tokyo",  "flightdate1": "23/10/2020", "flightdate2": "24/10/2020", "fds": "economy  class", "price": "$1100","bgimgurl": "tokyo"},
+     {"city": "madrid",  "flightdate1": "23/10/2020", "flightdate2": "24/10/2020", "fds": "first  class","price": "$5100","bgimgurl": "madrid"},
+     {"city": "madrid",  "flightdate1": "23/10/2020","flightdate2": "24/10/2020", "fds": "business  class", "price": "#1900","bgimgurl": "madrid"},
+     {"city": "madrid",  "flightdate1": "23/10/2020","flightdate2": "24/10/2020", "fds": "ecocnomy  class", "price": "$1100","bgimgurl": "madrid"},
+     {"city": "Séoul",  "flightdate1": "23/10/2020", "flightdate2": "24/10/2020", "fds": "first  class", "price": "$3100","bgimgurl": "Séoul"},
+    {"city": "Séoul",  "flightdate1": "23/10/2020","flightdate2": "24/10/2020", "fds": "business  class","price": "$2100","bgimgurl": "Séoul"},
+    {"city": "Séoul",  "flightdate1": "23/10/2020", "flightdate2": "24/10/2020", "fds": "economy  class", "price": "$850","bgimgurl": "Séoul"},
+     {"city": "new york",  "flightdate1": "23/10/2020", "flightdate2": "24/10/2020", "fds": "first  class", "price": "$4100","bgimgurl": "new york"},
+    {"city": "new york",  "flightdate1": "23/10/2020", "flightdate2": "24/10/2020", "fds": "business  class", "price": "$3600","bgimgurl": "new york"},
+    {"city": "new york",  "flightdate1": "23/10/2020", "flightdate2": "24/10/2020", "fds": "first  class", "price": "$900","bgimgurl": "new york"} ]
 
 
     
